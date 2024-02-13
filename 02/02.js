9dcb7d2f39a024c04bb60f67f7bd996ff525c3a9
alirezaGodratian <ma4a.gh@gmail.com>
2024-02-13 16:11:07 +0330

    new work 02

---
+++ 02/02.js
@@ -0,0 +1,26 @@
+ let input = [];
+
+ process.argv.forEach(x)
+
+ function x( val , index)
+ {
+    if(index >1)
+    {input[index-2]=val}
+
+ }
+
+ console.log(input)
+
+
+
+ let input2 = []
+
+ for (let index = 2; index <10; index++) 
+ {
+  input2[index-2]=process.argv[index]
+    
+ }
+
+ console.log(input2)
+
+
